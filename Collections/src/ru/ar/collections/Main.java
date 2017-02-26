277d0f4bc1bf446c76c32e6512b06c37c121122f
User <User@Computer.Dlink>
2017-02-26 17:35:25 +0500

    Создание java проект

---
+++ Collections/src/ru/ar/collections/Main.java
@@ -0,0 +1,20 @@
+package ru.ar.collections;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+	private static ArrayList<Mobiles> mobiles = new ArrayList<Mobiles>() ;
+	private static Random r = new Random();
+		public static void main(String[] args) {
+		 	for(int i=0;i<300;i++){
+		 		mobiles.add(new Mobiles(r.nextInt(9999999), "aaaaaaaa"));
+		 	}
+		 	for(Mobiles m: mobiles){
+		 		System.out.println(m.getNumber()+ "\t\t"+ m.getName());
+		 	}
+		}
+	
+	
+
+}
